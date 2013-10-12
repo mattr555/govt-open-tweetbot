0495beba78ad47d8a293e95fc8dacaaf66c6ac67
Matthew Ramina <mattramina@gmail.com>
2013-10-12 17:34:15 -0400

    Added license, tabs -> spaces

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import jinja2
 import os
@@ -24,20 +8,20 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), a
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
-    	self.response.write(*a, **kw)
+        self.response.write(*a, **kw)
     def render_str(self, template, **params):
-    	t = jinja_env.get_template(template)
-    	return t.render(params)
+        t = jinja_env.get_template(template)
+        return t.render(params)
     def render(self, template, **kw):
-    	self.write(self.render_str(template, **kw))
+        self.write(self.render_str(template, **kw))
 
 def govt_open():
-	u = urllib.urlopen('http://isthegovernmentopenyet.com').read()
-	return not 'Nope' in u
+    u = urllib.urlopen('http://isthegovernmentopenyet.com').read()
+    return not 'Nope' in u
 
 class MainHandler(Handler):
-	def get(self):
-		self.render('home.html', is_open=govt_open())
+    def get(self):
+        self.render('home.html', is_open=govt_open())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
